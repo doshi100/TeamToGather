f1197699f6ffeaa99d30d2984037a4d19f55c69f
עידן שמי <doshi050@walla.co.il>
2020-09-04 00:06:21 +0300

    added Constants.cs and DBHelper in order to query the database

---
+++ TeamTogather/DAL/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMedDAL
+{
+    /// <summary>
+    /// Constants
+    /// </summary>
+    public class Constants
+    {
+        public static string PROVIDER = @"Microsoft.ACE.OLEDB.12.0";
+        public static string PATH;
+        public  Constants(string path)
+        {
+            PATH = path;
+        }
+    }
+}
