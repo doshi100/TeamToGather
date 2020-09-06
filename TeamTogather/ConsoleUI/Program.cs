a36fcdec8b5621543774e23106309b01ed80d8a3
עידן שמי <doshi050@walla.co.il>
2020-09-07 01:21:28 +0300

    changed the authentication methods a bit, and added tips in program.cs for the continuation.

--- TeamTogather/ConsoleUI/Program.cs
+++ TeamTogather/ConsoleUI/Program.cs
@@ -20,6 +20,8 @@ namespace ConsoleUI
 
             /* things to remember :
                use 1 session that stores the user id after authentication and close it after 30 min.
+               add a class named Project, that contains the project fields.
+               add a list of projects to UserInfo ----> List<Projects>
                AppDomain.CurrentDomain.BaseDirectory gets me the base file which the program run on
                application domain is a mechanism like a process
              */
