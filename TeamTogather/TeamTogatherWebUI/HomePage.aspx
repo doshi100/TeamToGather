c97776834274d8eb3bdbe6d37617bcd4c90bf6b7
Idan Shami <doshi050@walla.co.il>
2020-09-10 12:25:46 +0300

    added a master page and a login page

---
+++ TeamTogather/TeamTogatherWebUI/HomePage.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/TeamTogatherMaster.Master" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="TeamTogatherWebUI.HomePage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>
+        <span>Username: </span>
+        <asp:TextBox ID="UserNameBox" runat="server"></asp:TextBox>
+    </p>
+    <span>Password: </span>
+    <asp:TextBox ID="PassBox" runat="server"></asp:TextBox>
+    <br/>
+    <asp:Button ID="LoginButton" runat="server" Text="login" OnClick="LoginButton_Click" />
+    <br />
+    <asp:Label ID="LoginMessage" runat="server" Text="Label"></asp:Label>
+
+</asp:Content>
---
+++ TeamTogather/TeamTogatherWebUI/HomePage.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BL;
+
+namespace TeamTogatherWebUI
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            string userName = UserNameBox.Text;
+            string password = PassBox.Text;
+            UserInfo user = UserInfo.Authentication(userName, password);
+            if(user != null)
+            {
+                if(user.Password == password && user.UserName == userName)
+                {
+                    Session["UserID"] = user.ID;
+                    LoginMessage.Text = "logged in !";
+                    user.UpdateLoginDate();
+                }
+            }
+            else
+            {
+                LoginMessage.Text = "the login failed";
+            }
+        }
+    }
+}
