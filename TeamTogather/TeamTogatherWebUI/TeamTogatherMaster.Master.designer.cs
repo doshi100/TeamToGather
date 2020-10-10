5bee3a7fb38411cfd85575e7e6f4a9e26a194b17
Idan <doshi050@walla.co.il>
2020-10-10 13:14:21 +0300

    added basic navbar

--- TeamTogather/TeamTogatherWebUI/TeamTogatherMaster.Master.designer.cs
+++ TeamTogather/TeamTogatherWebUI/TeamTogatherMaster.Master.designer.cs
@@ -31,6 +31,24 @@ namespace TeamTogatherWebUI {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// navbar1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl navbar1;
+        
+        /// <summary>
+        /// Div2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Div2;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
